663df35223b45bdf0c1005a981e94bcaceee70b7
Leandro Souza <leandrobonfimdesouza10@gmail.com>
2023-11-23 13:45:05 -0300

    Criação da programação em Angular

--- src/app/module/home/components/todo-list/todo-list.component.ts
+++ src/app/module/home/components/todo-list/todo-list.component.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 
+//Interface
+import { TaskList } from '../../model/task-list';
+
 @Component({
   selector: 'app-todo-list',
   templateUrl: './todo-list.component.html',
@@ -7,4 +10,10 @@ import { Component } from '@angular/core';
 })
 export class TodoListComponent {
 
+    public taskList: Array <TaskList> = [
+      {task: "Minha tarefa", checked: true},
+      {task: "Minha tarefa 2", checked: false}
+    ];
+
+    constructor(){}
 }
